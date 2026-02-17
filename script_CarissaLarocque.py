43425d01eef064ac3aae14fd164008d884c6e7cc
CARI-L <86850584+CARI-L@users.noreply.github.com>
2026-02-17 01:52:30 -0500

    spam input + metrics output

--- script_CarissaLarocque.py
+++ script_CarissaLarocque.py
@@ -4,12 +4,14 @@ from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.naive_bayes import ComplementNB
+from sklearn.naive_bayes import MultinomialNB, ComplementNB
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import pandas as pd
+
 
 # fetch dataset 
 youtube_spam_collection = fetch_ucirepo(id=380) 
@@ -59,7 +61,12 @@ cNB = Pipeline([
 ])
 cNB.fit(X_train, y_train)
 pred["cNB"] = cNB.predict(X_test)
-
+print("===========\nNaive Bayes\n===========")
+print("Type:      Multinomial  Complement\n----------------------------------")
+print("Accuracy:  %.3f        %.3f" %(accuracy_score(y_test, pred["mNB"]), accuracy_score(y_test, pred["cNB"])))
+print("Precision: %.3f        %.3f" %(precision_score(y_test, pred["mNB"]), precision_score(y_test, pred["cNB"])))
+print("Recall:    %.3f        %.3f" %(recall_score(y_test, pred["mNB"]), recall_score(y_test, pred["cNB"])))
+print("F1:        %.3f        %.3f\n\n" %(f1_score(y_test, pred["mNB"]), f1_score(y_test, pred["cNB"])))
 
 # SVM
 dSVM = Pipeline([
@@ -76,34 +83,124 @@ lSVM = Pipeline([
 lSVM.fit(X_train, y_train)
 pred["lSVM"] = lSVM.predict(X_test)
 
+print("=======================\nSupport Vector Machines\n=======================")
+print("Kernel:    Linear  RBF\n------------------------")
+print("Accuracy:  %.3f   %.3f" %(accuracy_score(y_test, pred["lSVM"]), accuracy_score(y_test, pred["dSVM"])))
+print("Precision: %.3f   %.3f" %(precision_score(y_test, pred["lSVM"]), precision_score(y_test, pred["dSVM"])))
+print("Recall:    %.3f   %.3f" %(recall_score(y_test, pred["lSVM"]), recall_score(y_test, pred["dSVM"])))
+print("F1:        %.3f   %.3f\n\n" %(f1_score(y_test, pred["lSVM"]), f1_score(y_test, pred["dSVM"])))
+
+
 # KNearestNeighbours
+print("====================\nK-Nearest Neighbours\n====================")
+print("K:         ", end="")
+
 k_range = range(3, 8)
 for k in k_range:
+    print(k, "     ", end="")
     neigh = Pipeline([
         ('prep', preprocess),
         ('clf', KNeighborsClassifier(n_neighbors=k))
     ])
     neigh.fit(X_train, y_train)
     pred[f"neigh{k}"] = neigh.predict(X_test)
+print("\n--------------------------------------------")
+print("Accuracy:  %.3f  %.3f  %.3f  %.3f  %.3f" %(accuracy_score(y_test, pred["neigh3"]), accuracy_score(y_test, pred["neigh4"]), accuracy_score(y_test, pred["neigh5"]), accuracy_score(y_test, pred["neigh6"]), accuracy_score(y_test, pred["neigh7"])))
+print("Precision: %.3f  %.3f  %.3f  %.3f  %.3f" %(precision_score(y_test, pred["neigh3"]), precision_score(y_test, pred["neigh4"]), precision_score(y_test, pred["neigh5"]), precision_score(y_test, pred["neigh6"]), precision_score(y_test, pred["neigh7"])))
+print("Recall:    %.3f  %.3f  %.3f  %.3f  %.3f" %(recall_score(y_test, pred["neigh3"]), recall_score(y_test, pred["neigh4"]), recall_score(y_test, pred["neigh5"]), recall_score(y_test, pred["neigh6"]), recall_score(y_test, pred["neigh7"])))
+print("F1:        %.3f  %.3f  %.3f  %.3f  %.3f\n\n" %(f1_score(y_test, pred["neigh3"]), f1_score(y_test, pred["neigh4"]), f1_score(y_test, pred["neigh5"]), f1_score(y_test, pred["neigh6"]), f1_score(y_test, pred["neigh7"])))
+
 
 # Artificial Neural Network
-mlp = Pipeline([
+mlp1 = Pipeline([
     ('prep', preprocess),
     ('clf', MLPClassifier(max_iter=500, random_state=42, early_stopping=True))
 ])
-mlp.fit(X_train, y_train)
-pred["mlp"] = mlp.predict(X_test)
+mlp1.fit(X_train, y_train)
+pred["mlp1"] = mlp1.predict(X_test)
+
+mlp2 = Pipeline([
+    ('prep', preprocess),
+    ('clf', MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=500, random_state=42, early_stopping=True))
+])
+mlp2.fit(X_train, y_train)
+pred["mlp2"] = mlp2.predict(X_test)
+
+print("==========================\nArtificial Neural Network\n==========================")
+print("Layers:    (100,)  (100, 50)\n----------------------------")
+print("Accuracy:  %.3f   %.3f" %(accuracy_score(y_test, pred["mlp1"]), accuracy_score(y_test, pred["mlp2"])))
+print("Precision: %.3f   %.3f" %(precision_score(y_test, pred["mlp1"]), precision_score(y_test, pred["mlp2"])))
+print("Recall:    %.3f   %.3f" %(recall_score(y_test, pred["mlp1"]), recall_score(y_test, pred["mlp2"])))
+print("F1:        %.3f   %.3f\n\n" %(f1_score(y_test, pred["mlp1"]), f1_score(y_test, pred["mlp2"])))
 
 # Random Forest (Test N)
+print("==============\nRandom Forests\n==============")
+print("Estimators: 100                       200                       500")
+print("Max Depth:  ", end="")
 test_params = {
-    "n_estimators": [50, 100, 200],
-    "max_depth": [None, 20, 40]
+    "n_estimators": [100, 200, 500],
+    "max_depth": [20, 40, None]
 }
 for n in test_params["n_estimators"]:
     for d in test_params["max_depth"]:  
         forest = Pipeline([
             ('prep', preprocess),
-            ('clf', RandomForestClassifier())
+            ('clf', RandomForestClassifier(max_depth=d, n_estimators=n, random_state=42))
         ])
         forest.fit(X_train, y_train)
         pred[f"forest{n}-{d}"] = forest.predict(X_test)
+        print(d,"     ", end="")
+print("\n-------------------------------------------------------------------------------------")
+print("Accuracy:   %.3f   %.3f   %.3f     %.3f   %.3f   %.3f     %.3f   %.3f   %.3f" %(accuracy_score(y_test, pred["forest100-20"]), accuracy_score(y_test, pred["forest100-40"]), accuracy_score(y_test, pred["forest100-None"]), accuracy_score(y_test, pred["forest200-20"]), accuracy_score(y_test, pred["forest200-40"]), accuracy_score(y_test, pred["forest200-None"]), accuracy_score(y_test, pred["forest500-20"]), accuracy_score(y_test, pred["forest500-40"]), accuracy_score(y_test, pred["forest500-None"])))
+print("Precision:  %.3f   %.3f   %.3f     %.3f   %.3f   %.3f     %.3f   %.3f   %.3f" %(precision_score(y_test, pred["forest100-20"]), precision_score(y_test, pred["forest100-40"]), precision_score(y_test, pred["forest100-None"]), precision_score(y_test, pred["forest200-20"]), precision_score(y_test, pred["forest200-40"]), precision_score(y_test, pred["forest200-None"]), precision_score(y_test, pred["forest500-20"]), precision_score(y_test, pred["forest500-40"]), precision_score(y_test, pred["forest500-None"])))
+print("Recall:     %.3f   %.3f   %.3f     %.3f   %.3f   %.3f     %.3f   %.3f   %.3f" %(recall_score(y_test, pred["forest100-20"]), recall_score(y_test, pred["forest100-40"]), recall_score(y_test, pred["forest100-None"]), recall_score(y_test, pred["forest200-20"]), recall_score(y_test, pred["forest200-40"]), recall_score(y_test, pred["forest200-None"]), recall_score(y_test, pred["forest500-20"]), recall_score(y_test, pred["forest500-40"]), recall_score(y_test, pred["forest500-None"])))
+print("F1:         %.3f   %.3f   %.3f     %.3f   %.3f   %.3f     %.3f   %.3f   %.3f\n\n" %(f1_score(y_test, pred["forest100-20"]), f1_score(y_test, pred["forest100-40"]), f1_score(y_test, pred["forest100-None"]), f1_score(y_test, pred["forest200-20"]), f1_score(y_test, pred["forest200-40"]), f1_score(y_test, pred["forest200-None"]), f1_score(y_test, pred["forest500-20"]), f1_score(y_test, pred["forest500-40"]), f1_score(y_test, pred["forest500-None"])))
+
+print("===============\nThe Best Models\n===============")
+print("Model Type: Naive Bayes  SVM    Nearest Neighbours  Neural Network  Random Forest\n---------------------------------------------------------------------------------")
+print("Accuracy:   %.3f        %.3f  %.3f               %.3f           %.3f" %(accuracy_score(y_test, pred["cNB"]), accuracy_score(y_test, pred["lSVM"]), accuracy_score(y_test, pred["neigh3"]), accuracy_score(y_test, pred["mlp1"]), accuracy_score(y_test, pred["forest100-20"])))
+print("Precision:  %.3f        %.3f  %.3f               %.3f           %.3f" %(precision_score(y_test, pred["cNB"]), precision_score(y_test, pred["lSVM"]), precision_score(y_test, pred["neigh3"]), precision_score(y_test, pred["mlp1"]), precision_score(y_test, pred["forest100-20"])))
+print("Recall:     %.3f        %.3f  %.3f               %.3f           %.3f" %(recall_score(y_test, pred["cNB"]), recall_score(y_test, pred["lSVM"]), recall_score(y_test, pred["neigh3"]), recall_score(y_test, pred["mlp1"]), recall_score(y_test, pred["forest100-20"])))
+print("F1:         %.3f        %.3f  %.3f               %.3f           %.3f\n\n" %(f1_score(y_test, pred["cNB"]), f1_score(y_test, pred["lSVM"]), f1_score(y_test, pred["neigh3"]), f1_score(y_test, pred["mlp1"]), f1_score(y_test, pred["forest100-20"])))
+
+# print confusion matrices for best models
+
+# test tool
+neigh = Pipeline([
+    ('prep', preprocess),
+    ('clf', KNeighborsClassifier(n_neighbors=3))
+])
+neigh.fit(X_train, y_train)
+
+forest = Pipeline([
+    ('prep', preprocess),
+    ('clf', RandomForestClassifier(n_estimators=200, random_state=42))
+])
+forest.fit(X_train, y_train)
+
+username = ""
+while True:
+    name = input("User Name (leave blank to reuse): ")
+    if username == "": username = name
+    while username == "":
+        print("User Name not set, please set username")
+        username = input("User Name: ")
+    content = ""
+    content = input("Write your comment:\n")
+    while content == "":
+        print("Content of comment cannot be empty")
+        content = input("Write your comment:\n")
+    print()
+    
+    # create dataframe
+    data = {"AUTHOR": [username], "CONTENT": [content]}
+    df = pd.DataFrame.from_dict(data)
+    print("Naive Bayes says: THIS IS %sSPAM" %("NOT " if cNB.predict(df)[0] == 0 else ""))
+    print("SVM says: THIS IS %sSPAM" %("NOT " if lSVM.predict(df)[0] == 0 else ""))
+    print("Nearest Neighbours says: THIS IS %sSPAM" %("NOT " if neigh.predict(df)[0] == 0 else ""))
+    print("Artificial Neural Network says: THIS IS %sSPAM" %("NOT " if mlp1.predict(df)[0] == 0 else ""))
+    print("Random Forest says: THIS IS %sSPAM" %("NOT " if forest.predict(df)[0] == 0 else ""))
+    q = input("\nQuit? (y/n): ").lower()
+    if q == "y":
+        break
+    print()
